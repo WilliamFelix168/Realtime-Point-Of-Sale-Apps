ec091960cefa1f363175e3f9aa4e3165bcfb8fd6
William Felix <william.felix059@gmail.com>
2026-09-05 10:47:27 +0700

    feat: add login form validation

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -1,12 +1,99 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+
+import { Input } from "@/components/ui/input";
+import { INITIAL_LOGIN_FORM } from "@/constansts/auth-constant";
+import { LoginForm, loginSchema } from "@/validations/auth-validation";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 
 export default function Login() {
+  const form = useForm<LoginForm>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: INITIAL_LOGIN_FORM,
+  });
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    console.log(data);
+  });
+
   return (
     <Card>
       <CardHeader className="text-center">
         <CardTitle className="text-xl">Welcome</CardTitle>
+
         <CardDescription>Login to access all features</CardDescription>
       </CardHeader>
+
+      <CardContent>
+        <form onSubmit={onSubmit}>
+          <FieldGroup>
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+
+                  <Input
+                    {...field}
+                    id={field.name}
+                    type="email"
+                    placeholder="Insert your email"
+                    autoComplete="off"
+                    aria-invalid={fieldState.invalid}
+                  />
+
+                  {fieldState.error && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+
+                  <Input
+                    {...field}
+                    id={field.name}
+                    type="password"
+                    placeholder="******"
+                    autoComplete="off"
+                    aria-invalid={fieldState.invalid}
+                  />
+
+                  {fieldState.error && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Button type="submit">Login</Button>
+          </FieldGroup>
+        </form>
+      </CardContent>
     </Card>
   );
 }
